73d9128a14104e06a6fc66d23a1ecb4bab315e82
Shuhey <shuhey.dev@gmail.com>
2020-06-04 17:35:55 +0900

    Add MainPageViewModel and Binding.

---
+++ XFMvvmHelperPractice/XFMvvmHelperPractice/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using XFMvvmHelperPractice.Models;
+
+namespace XFMvvmHelperPractice.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        private ObservableRangeCollection<Person> _people;
+        public ObservableRangeCollection<Person> Person
+        {
+            get => _people;
+            set => SetProperty(ref _people, value);
+        }
+
+        public MainPageViewModel()
+        {
+            //テスト
+            this.Title = "Hello MvvmHelper";
+            this.SubTitle = "How to use MvvmHelper";
+            this.IsBusy = true;
+        }
+    }
+}
