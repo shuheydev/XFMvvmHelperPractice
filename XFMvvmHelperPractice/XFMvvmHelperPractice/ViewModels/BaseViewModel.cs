33a5fa71ade7beacbcc37c7d3436d999d1b8c471
Shuhey <shuhey.dev@gmail.com>
2020-06-04 16:46:39 +0900

    Added BaseViewModel.

---
+++ XFMvvmHelperPractice/XFMvvmHelperPractice/ViewModels/BaseViewModel.cs
@@ -0,0 +1,56 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFMvvmHelperPractice.ViewModels
+{
+    public class BaseViewModel : ObservableObject
+    {
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        private string _subTitle;
+        public string SubTitle
+        {
+            get => _subTitle;
+            set => SetProperty(ref _subTitle, value);
+        }
+
+        private string _icon;
+        public string Icon
+        {
+            get => _icon;
+            set => SetProperty(ref _icon, value);
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                if (SetProperty(ref _isBusy, value))
+                    IsNotBusy = !_isBusy;
+            }
+        }
+
+        private bool _isNotBusy;
+        public bool IsNotBusy
+        {
+            get => _isNotBusy;
+            set => SetProperty(ref _isNotBusy, value);
+        }
+
+        private bool _canLoadMore;
+        public bool CanLoadMore
+        {
+            get => _canLoadMore;
+            set => SetProperty(ref _canLoadMore, value);
+        }
+    }
+}
